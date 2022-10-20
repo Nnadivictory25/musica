8e151ffd049cc23f3bc0d4181205f3d64575a32e
Nnadi Victory <chikamagozi25@gmail.com>
2022-10-20 21:48:03 +0100

    fixed mobile navbar toggle

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,30 @@
-console.log('hello world');
+const openIcon = document.querySelector('#openIcon')
+const closeIcon = document.querySelector('#closeIcon')
+const togglers = document.querySelectorAll('.toggler')
+const body = document.querySelector('body')
+const nav = document.querySelector('.mobile-nav')
+
+let navActive = false;
+
+togglers.forEach(toggleBtn => {
+
+    toggleBtn.addEventListener('click', () => {
+        nav.classList.remove('slide-out')
+        if (!navActive) {
+            openIcon.classList.add('hide')
+            closeIcon.classList.remove('hide')
+            nav.classList.add('slide-in')
+            body.classList.add('overflow-hide')
+            navActive = !navActive
+        } else if(navActive) {
+            openIcon.classList.remove('hide')
+            closeIcon.classList.add('hide')
+            nav.classList.remove('slide-in')
+            nav.classList.add('slide-out')
+            body.classList.remove('overflow-hide')
+            navActive = !navActive
+        }
+
+    })
+})
+
